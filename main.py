0eb86f97738da77af7f186dd5467d5a799620c8d
giffin-mh-osborne <giffin.mh.osborne@gmail.com>
2023-02-17 13:05:26 -0500

    reduced requirements

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, url_for, redirect, render_template, jsonify
 from pycaret.regression import *
-import csv
 import pandas as pd
-import pickle
 import numpy as np
 
 app = Flask(__name__, template_folder='pages')
